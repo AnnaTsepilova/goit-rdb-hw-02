d2688d1dded0de66b9995a875b8073a257266f93
Anna Tsepilova <virgo.tsav@gmail.com>
2024-03-21 13:24:09 +0200

    add db files

---
+++ diagram.sql
@@ -0,0 +1,83 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`customer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`customer` (
+  `customer_id` INT NOT NULL,
+  `customer_name` VARCHAR(45) NOT NULL,
+  `customer_address` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE INDEX `customer_id_UNIQUE` (`customer_id` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`order`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`order` (
+  `order_id` INT NOT NULL,
+  `customer_id` INT NOT NULL,
+  `order_date` DATETIME NOT NULL,
+  PRIMARY KEY (`order_id`),
+  UNIQUE INDEX `order_id_UNIQUE` (`order_id` ASC) VISIBLE,
+  UNIQUE INDEX `customer_id_UNIQUE` (`customer_id` ASC) VISIBLE,
+  CONSTRAINT `FK_customer`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `mydb`.`customer` (`customer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`order_item`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`order_item` (
+  `order_id` INT NOT NULL,
+  `product_id` INT NOT NULL,
+  `quantity` INT NOT NULL,
+  PRIMARY KEY (`order_id`),
+  UNIQUE INDEX `PK_order_id_UNIQUE` (`order_id` ASC) VISIBLE,
+  UNIQUE INDEX `product_id_UNIQUE` (`product_id` ASC) VISIBLE,
+  CONSTRAINT `FK_order`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `mydb`.`order` (`order_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`product`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`product` (
+  `product_id` INT NOT NULL,
+  `product_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`product_id`),
+  UNIQUE INDEX `good_id_UNIQUE` (`product_id` ASC) VISIBLE,
+  CONSTRAINT `FK_order_item`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `mydb`.`order_item` (`product_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
